c04319a58d0576c588c28679fce25679db1f6487
Shivam Sharma <123826841+Shivam-sharmag@users.noreply.github.com>
2023-02-12 19:58:26 +0530

    Add files via upload

---
+++ cred.py
@@ -0,0 +1,22 @@
+#Twilio Details
+
+account_sid = 'AC06fdf1b360ac37634c8f403c7bdd335d'
+auth_token = 'c75abd50cae337ff24434f4a533395eb'
+twilionumber = '+16018846446'
+twiliosmsnumber = '+16018846446'
+
+#FC Bot
+API_TOKEN = "5856195740:AAFaDbvuIYUfppYSXuG146BJbSiZZ1ACZ4g"
+
+#Host URL
+callurl = 'https://e836-196-190-61-63.eu.ngrok.io'
+twiliosmsurl = 'https://e836-196-190-61-63.eu.ngrok.io/sms'
+
+
+
+
+
+
+
+
+
